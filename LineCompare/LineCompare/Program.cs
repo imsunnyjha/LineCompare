83f03be2aa373dbc11db9e88e1a2198fbf421f9e
Sunny Jha <sunnyjha11897@gmail.com>
2020-09-27 00:39:13 +0530

    [Sunny] Add. Welcome To Line Compare Problem

---
+++ LineCompare/LineCompare/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LineCompare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Line Compare Computation Problem!");
+        }
+    }
+}
